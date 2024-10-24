ff0932fc7d209596344303e5ce9599706e9e8efe
Mano Bouttelisier <mano.bouttelisier@docents-iMac.local>
2024-10-24 13:25:48 +0200

    first commit

---
+++ WKQatarApp/WKQatarAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WKQatarAppApp.swift
+//  WKQatarApp
+//
+//  Created by Mano Bouttelisier on 24/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct WKQatarAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WKQatarAppTests/WKQatarAppTests.swift
@@ -0,0 +1,16 @@
+//
+//  WKQatarAppTests.swift
+//  WKQatarAppTests
+//
+//  Created by Mano Bouttelisier on 24/10/2024.
+//
+
+import Testing
+
+struct WKQatarAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
